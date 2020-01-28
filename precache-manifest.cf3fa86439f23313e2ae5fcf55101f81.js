115c4d2f8f349b616883227abfcc4aae094cec9b
rsaied <roukaya.saied@talan.com>
2020-01-28 17:03:49 +0100

    Updates

---
+++ precache-manifest.cf3fa86439f23313e2ae5fcf55101f81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faab6b2b0c07a8ced2c475c6431fb1b5",
+    "url": "/Morpion-Game/index.html"
+  },
+  {
+    "revision": "adaaf8dddd37381f8019",
+    "url": "/Morpion-Game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "de0a39f902bee61bba77",
+    "url": "/Morpion-Game/static/css/main.98113401.chunk.css"
+  },
+  {
+    "revision": "adaaf8dddd37381f8019",
+    "url": "/Morpion-Game/static/js/2.d93aa206.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Morpion-Game/static/js/2.d93aa206.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de0a39f902bee61bba77",
+    "url": "/Morpion-Game/static/js/main.380c839e.chunk.js"
+  },
+  {
+    "revision": "83115f2e5fb21b6a05c8",
+    "url": "/Morpion-Game/static/js/runtime-main.8e854936.js"
+  }
+]);
